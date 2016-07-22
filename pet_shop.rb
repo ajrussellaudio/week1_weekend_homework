2da3a5102ce7990cfb806b9714ab3b6a208370df
Alan Russell <ajrussellaudio@gmail.com>
2016-07-22 11:36:04 +0100

    increases pets sold

--- pet_shop.rb
+++ pet_shop.rb
@@ -6,10 +6,15 @@ def total_cash(shop)
   return shop[:admin][:total_cash]
 end
 
-def add_or_remove_cash(shop, transaction)
-  shop[:admin][:total_cash] += transaction
+def add_or_remove_cash(shop, sale_value)
+  shop[:admin][:total_cash] += sale_value
 end
 
 def pets_sold(shop)
   return shop[:admin][:pets_sold]
 end
+
+def increase_pets_sold(shop, number_of_pets_sold)
+  shop[:admin][:pets_sold] += number_of_pets_sold
+end
+
