33b1c02ed0065687dc0602c7c57e21bc25881c4c
Alan Russell <ajrussellaudio@gmail.com>
2016-07-22 13:26:42 +0100

    checks if customer can afford pet

--- pet_shop.rb
+++ pet_shop.rb
@@ -54,3 +54,13 @@ end
 def add_pet_to_customer(customer, pet)
   customer[:pets] << pet
 end
+
+# OPTIONAL
+
+def customer_can_afford_pet(customer, pet)
+  if customer[:cash] > pet[:price]
+    return true
+  else
+    return false
+  end
+end
